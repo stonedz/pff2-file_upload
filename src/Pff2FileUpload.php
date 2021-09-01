28ff067bc7d1dbfe6c14d345eca240b5c2d65c63
Paolo Fagni <paolo.fagni@gmail.com>
2021-09-01 12:14:45 +0200

    php-cs-fixer pass and update for php8.0

--- src/Pff2FileUpload.php
+++ src/Pff2FileUpload.php
@@ -2,7 +2,6 @@
 
 namespace pff\modules;
 
-
 use pff\Abs\AModule;
 use pff\Exception\PffException;
 use pff\Iface\IConfigurableModule;
@@ -11,23 +10,24 @@ use pff\Iface\IConfigurableModule;
  * Class Pff2FileUpload
  * @package pff\modules
  */
-class Pff2FileUpload extends AModule implements IConfigurableModule {
-
+class Pff2FileUpload extends AModule implements IConfigurableModule
+{
     private $fileDir;
     private $validMimeTypes;
 
-    public function __construct($confFile = 'pff2-file_upload/module.conf.local.yaml') {
+    public function __construct($confFile = 'pff2-file_upload/module.conf.local.yaml')
+    {
         $this->loadConfig($confFile);
-
     }
 
     /**
      * @param array $parsedConfig
      * @return mixed
      */
-    public function loadConfig($parsedConfig) {
-        $conf = $this->readConfig($parsedConfig);
-        $this->fileDir = ROOT. DS . 'app' . DS . 'public'. DS.$conf['moduleConf']['dir'] .DS;
+    public function loadConfig($parsedConfig)
+    {
+        $conf                 = $this->readConfig($parsedConfig);
+        $this->fileDir        = ROOT. DS . 'app' . DS . 'public'. DS.$conf['moduleConf']['dir'] .DS;
         $this->validMimeTypes = $conf['moduleConf']['validMimeTypes'];
     }
 
@@ -41,46 +41,43 @@ class Pff2FileUpload extends AModule implements IConfigurableModule {
      * @param null|string $filename name to be given to the uploaded file
      * @return bool|string
      */
-    public function saveFile($fileArray, $add_uniqueid = true, $dest = null, $filename = null) {
-        $tmp_file      = $fileArray['tmp_name'];
-        $name          = $fileArray['name'];
+    public function saveFile($fileArray, $add_uniqueid = true, $dest = null, $filename = null)
+    {
+        $tmp_file = $fileArray['tmp_name'];
+        $name     = $fileArray['name'];
 
-        if($dest && substr($dest, -1) != '/') {
+        if ($dest && substr($dest, -1) != '/') {
             $dest = $dest.'/';
         }
 
 
-        if($add_uniqueid) {
-            if($filename) {
+        if ($add_uniqueid) {
+            if ($filename) {
                 $new_name = uniqid().$filename;
-            }
-            else {
+            } else {
                 $new_name = uniqid().$name;
             }
-        }
-        else {
-            if($filename) {
-               $new_name = $filename;
-            }
-            else {
+        } else {
+            if ($filename) {
+                $new_name = $filename;
+            } else {
                 $new_name = $name;
             }
         }
 
         $this->createDirIfNotExist($dest);
 
-        if($dest) {
+        if ($dest) {
             $new_full_name = $this->fileDir.$dest.$new_name;
-        }
-        else {
+        } else {
             $new_full_name = $this->fileDir.$new_name;
         }
 
-        if(!$this->checkMimeType($fileArray['type'])) {
+        if (!$this->checkMimeType($fileArray['type'])) {
             return false;
         }
 
-        if(! move_uploaded_file($tmp_file, $new_full_name)) {
+        if (! move_uploaded_file($tmp_file, $new_full_name)) {
             throw new PffException('Error uploading the file', 500);
         }
 
@@ -93,11 +90,11 @@ class Pff2FileUpload extends AModule implements IConfigurableModule {
      * @param $dest_dir
      * @return bool
      */
-    private function createDirIfNotExist($dest_dir) {
-        if(file_exists($this->fileDir.$dest_dir)) {
+    private function createDirIfNotExist($dest_dir)
+    {
+        if (file_exists($this->fileDir.$dest_dir)) {
             return true;
-        }
-        else {
+        } else {
             return mkdir($this->fileDir.$dest_dir, 0752);
         }
     }
@@ -107,11 +104,11 @@ class Pff2FileUpload extends AModule implements IConfigurableModule {
      * @param string $fileMimeType
      * @return bool
      */
-    public function checkMimeType($fileMimeType) {
-        if(in_array($fileMimeType, $this->validMimeTypes)) {
+    public function checkMimeType($fileMimeType)
+    {
+        if (in_array($fileMimeType, $this->validMimeTypes)) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
@@ -120,15 +117,13 @@ class Pff2FileUpload extends AModule implements IConfigurableModule {
      * @param string $fileName
      * @return bool
      */
-    public function deleteFile($fileName) {
-
-        if(file_exists($this->fileDir.$fileName)) {
+    public function deleteFile($fileName)
+    {
+        if (file_exists($this->fileDir.$fileName)) {
             unlink($this->fileDir.$fileName);
             return true;
-        }
-        else {
+        } else {
             return false;
         }
-
     }
 }
